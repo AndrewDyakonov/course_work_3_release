4fa2553532d81847e491d412fa9c567f0f5f2d36
blendi <dronramone@mail.ru>
2023-02-18 23:16:04 +0600

    Создана функция create_ex_class

--- funktion.py
+++ funktion.py
@@ -1,5 +1,5 @@
 import json
-
+from classes import Operation
 
 def get_json_to_list():
     """
@@ -11,3 +11,29 @@ def get_json_to_list():
     return list_operation
 
 
+def create_ex_class():
+    """
+    Создать список экземпляров класса
+    :return:
+    """
+    list_ex_class = []
+    list_from_json = get_json_to_list()
+    list_from_json.sort(key=lambda d: d['date'], reverse=True)
+    for i in list_from_json:
+        if 'from' in i.keys():
+            list_ex_class.append(Operation(i['date'],
+                                           i['description'],
+                                           i['to'],
+                                           i['operationAmount']['currency']['name'],
+                                           i['operationAmount']['amount'],
+                                           i['state'],
+                                           i['from']))
+
+        else:
+            list_ex_class.append(Operation(i['date'],
+                                           i['description'],
+                                           i['to'],
+                                           i['operationAmount']['currency']['name'],
+                                           i['operationAmount']['amount'],
+                                           i['state']))
+    return list_ex_class
