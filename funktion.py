1547f1fd3bbaf2c16adc1f17b54f57b07f7cd674
blendi <dronramone@mail.ru>
2023-02-18 23:09:53 +0600

    Добавлен htmlcov в gitignore. Изменения в функции get_json_to_list

--- funktion.py
+++ funktion.py
@@ -1,10 +1,11 @@
 import json
 
+
 def get_json_to_list():
     """
     Получить список операций из файла operation.json
     :return:
     """
-    with open('operation.json', 'r', encoding="UTF-8") as file:
+    with open('operations.json', 'r', encoding="UTF-8") as file:
         list_operation = json.load(file)
     return list_operation
